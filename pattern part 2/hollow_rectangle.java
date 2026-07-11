6f0ac44d3efa6b480ea9a6a49f8ddf4db88a3105
Kumaranubhavvv <kumaranubhav545@gmail.com>
2026-07-11 14:34:42 +0530

    sorting prctice

---
+++ pattern part 2/hollow_rectangle.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class hollow_rectangle{
+    public static void hollow_rectangle(int totRows,int totCols){
+        //outer loop
+        for(int i=1;i<=totRows;i++){
+
+            //inner-columns
+            for(int j=1;j<=totCols;j++){
+                if(i==1||i==totRows||j==1||j==totCols){
+                    //boundaries cells
+                    System.out.println("*");
+                }else{
+                    System.out.print("");
+                }
+            }
+            System.out.println();
+        }
+    }
+   public static void main(String args[]){
+    hollow_rectangle(4, 5);
+   }
+}
+
+    
+
---
+++ sorting/countingSort.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class CountingSort {
+
+    public static void countingSort(int arr[]) {
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+
+        for (int i = count.length - 1; i >= 0; i--) {
+
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+
+        countingSort(arr);
+        printArr(arr);
+    }
+}
